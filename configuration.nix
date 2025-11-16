0c25792c67d88e333b11838d7113f4b472787b62
dosluke <dosluke@gmail.com>
2025-11-16 04:48:32 -0500

    Auto-sync from nixos at 2025-11-16 04:48:32

--- configuration.nix
+++ configuration.nix
@@ -58,19 +58,23 @@
 
 
 
-  home-manager.backupFileExtension = "backup";
+}
 
-  home-manager.users.me = { pkgs, ... }: {
-      home.stateVersion = "25.05";
+{
+	home-manager.backupFileExtension = "backup";
+
+	home-manager.users.me = { pkgs, ... }: {
+	    home.stateVersion = "25.05";
 
 
-      home.file.".config/autostart/kitty.desktop".text = ''
-            [Desktop Entry]
-            Type=Application
-            Exec=kitty
-            Name=Kitty
-            X-KDE-autostart-phase=2
-          '';
-          
-    };
+	    home.file.".config/autostart/kitty.desktop".text = ''
+	          [Desktop Entry]
+	          Type=Application
+	          Exec=kitty
+	          Name=Kitty
+	          X-KDE-autostart-phase=2
+	        '';
+	        
+	  };
+
 }
